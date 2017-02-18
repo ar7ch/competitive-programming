e05e846ccffcb3497c5948592d0b82660e91c5a2
blgcyan <artmbulgakov@gmail.com>
2017-02-18 17:51:33 +0300

    added 208A solutions

---
+++ codeforces/208A.cpp
@@ -0,0 +1,56 @@
+/*
+208A
+Dubstep
+Vasya works as a DJ in the best Berland nightclub, and he often uses dubstep music in his performance. Recently, he has decided to take a couple of old songs and make dubstep remixes from them.
+
+Let's assume that a song consists of some number of words. To make the dubstep remix of this song, Vasya inserts a certain number of words "WUB" before the first word of the song (the number may be zero), after the last word (the number may be zero), and between words (at least one between any pair of neighbouring words), and then the boy glues together all the words, including "WUB", in one string and plays the song at the club.
+
+For example, a song with words "I AM X" can transform into a dubstep remix as "WUBWUBIWUBAMWUBWUBX" and cannot transform into "WUBWUBIAMWUBX".
+
+Recently, Petya has heard Vasya's new dubstep track, but since he isn't into modern music, he decided to find out what was the initial song that Vasya remixed. Help Petya restore the original song.
+
+Input
+The input consists of a single non-empty string, consisting only of uppercase English letters, the string's length doesn't exceed 200 characters. It is guaranteed that before Vasya remixed the song, no word contained substring "WUB" in it; Vasya didn't change the word order. It is also guaranteed that initially the song had at least one word.
+
+Output
+Print the words of the initial song that Vasya used to make a dubsteb remix. Separate the words with a space.
+*/
+/* Accepted 30ms 2052 KB */
+
+
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+std::vector<std::string> split(const std::string& str, const std::string& delim)
+{
+    using namespace std;
+    vector<string> tokens;
+    size_t prev = 0, pos = 0;
+    do
+    {
+        pos = str.find(delim, prev);
+        if (pos == string::npos) pos = str.length();
+        string token = str.substr(prev, pos-prev);
+        if (!token.empty()) tokens.push_back(token);
+        prev = pos + delim.length();
+    }
+    while (pos < str.length() && prev < str.length());
+    return tokens;
+}
+
+int main()
+{
+  std::string song;
+  std::cin >> song;
+  std::vector<std::string> v = split(song, "WUB");
+  for(std::vector<std::string>::iterator it=v.begin(); it!=v.end(); ++it){
+    if(*it != ""){
+      if(it != v.end()-1)
+        std::cout << *it << " ";
+      else
+        std::cout << *it << '\n';
+    }
+  }
+  return 0;
+}
